db9d51d68298ea42f8389a2c149b7235ec524171
Marco Silva <marcosilva@MacBook-Air-de-Marco.local>
2023-03-09 11:05:22 -0300

    Update flagst 3

--- course.php
+++ course.php
@@ -1,3 +1,18 @@
 <?php
+use \PDO;
+use \PDOException;
+$host = 'mysql'; //Se o banco de dados estiver no mesmo servidor deste arquivo, use localhost
+$database = 'default';
+$user = 'root';
+$password = 'root';
+$charset = 'utf8';
 
-var_dump($_SERVER);
+try {
+    //code...
+    $banco = new PDO("mysql:host={$host};dbname={$database}",$user,$password);
+    echo "Conexão realizada com sucesso";
+} catch (PDOException $e) {
+    //throw $th;
+    echo $e->getMessage();
+    
+}
